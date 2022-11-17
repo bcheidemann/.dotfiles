b923eb0816b1d7549134aeafb726352eb649d137
Ben Heidemann <ben@heidemann.co.uk>
2022-11-17 18:17:09 +0000

    add .aliases install instructions to install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ ln -s $BASEDIR/.gitignore $HOME/.gitignore
 echo "Configuring git core.excludesfile..."
 git config --global core.excludesfile $HOME/.gitignore
 
+echo ""
+
+# Global .aliases
+echo "Please add the following line to your .bashrc or .zshrc file:"
+echo " export ALIASES_SCRIPT_PATH=$BASEDIR/.aliases"
+echo " source \$ALIASES_SCRIPT_PATH"
 
 echo ""
 echo "Done."
