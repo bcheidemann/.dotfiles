a7d740e04bda21f8c36e1df80074940a2ba7c4d1
Ben Heidemann <ben@heidemann.co.uk>
2022-11-24 18:11:48 +0000

    configure buffer switch shortcut and terminal for nvim

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -29,8 +29,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<C-K>", ":bnext<CR>", opts)
+keymap("n", "<C-J>", ":bprevious<CR>", opts)
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
--- .config/nvim/lua/user/toggleterm.lua
+++ .config/nvim/lua/user/toggleterm.lua
@@ -4,7 +4,15 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
+	size = function (term)
+    if term.direction == "float" then
+	    return 20
+    elseif term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then 
+      return vim.o.columns * 0.4
+	  end
+	end,
 	open_mapping = [[<c-\>]],
 	hide_numbers = true,
 	shade_terminals = true,
