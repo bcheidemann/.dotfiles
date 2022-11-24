9e9e6bae0366f7e3c0ef76f7af29720208a56b49
Ben Heidemann <ben@heidemann.co.uk>
2022-11-24 01:26:16 +0000

    configure vscode style ctrl+P and ctrl+shift+F for neovim

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -57,8 +57,9 @@ keymap("v", "p", '"_dP', opts)
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- Telescope
-keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+-- keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
+keymap("n", "<C-F>", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 
